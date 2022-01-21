55cfa2ee46242d55736303e79524572a9bd83184
yyeopp <letsksy@naver.com>
2022-01-21 12:35:40 +0900

    self

---
+++ test/분반 테스트.py
@@ -0,0 +1,83 @@
+# print(2 or 3)
+# print(len(range(-1,4,2)))
+
+# x=1
+# def outer():
+#     x=2
+#     def inner():
+#         x=3
+#     inner()
+#     print(x)
+# x=4
+# outer()
+
+
+# def fibonacci(n):
+#     if n<2:
+#         return n
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+# print(fibonacci(5))
+
+# x=1
+# result = 0
+# if x ==1:
+#     result = "hi"
+# else:
+#     result = "bye"
+
+
+# numbers = [1,2,3,4,5]
+# total = 0
+# for idx, number in enumerate(numbers):
+#     total = total + number + idx
+# print(total)
+#
+# a = 11//2
+# print(a)
+
+# name = "hong"
+# class Person:
+#     name = "choi"
+#     def greeting(self):
+#         print(self.name)
+# p1 = Person()
+# p1.name = "kim"
+# p1.greeting()
+#
+# list_1 = [1,2,3]
+# list_2 = list_1[:]
+# print(list_2)
+# list_1.append("a")
+# list_2.append("b")
+# print(list_1)
+# print(list_2)
+
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+# black = Cat()
+# black.name = "가망"
+
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+# class Mom(Person):
+#     gene = 'XX'
+# class Dad(Person):
+#     gene = 'XY'
+# class FirstChild(Dad, Mom):
+#     pass
+# baby = FirstChild('baby')
+# print(baby.gene)
+
+# list = [1,3,5,7]
+# list.pop()
+# print(sum(list))
+
+# a = 1
+# b = 2
+# c = 3
+# print(dir(a))
+
