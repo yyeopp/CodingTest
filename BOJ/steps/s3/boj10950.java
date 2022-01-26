52e6d45871b7129ad6d7969fefb491de096174e3
yyeopp <letsksy@naver.com>
2022-01-26 23:46:45 +0900

    0126

---
+++ BOJ/steps/s3/boj10950.java
@@ -0,0 +1,27 @@
+package steps.s3;
+
+import java.util.Scanner;
+
+public class boj10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int [][] set = new int[T][2];
+		
+		for (int i = 0; i < set.length; i++) {
+			for (int j = 0; j < set[i].length; j++) {
+				set[i][j] = sc.nextInt();
+			}
+		}
+		
+		sc.close();
+		
+		for (int i = 0; i < set.length; i++) {
+			System.out.println(set[i][0] + set[i][1]);
+		}
+		
+	}
+
+}
