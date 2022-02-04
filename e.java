9f37e6e84c99ee2b953ab5ad38627a5327bb3cb3
yyeopp <letsksy@naver.com>
2022-02-04 15:42:50 +0900

    2

--- e.java
+++ e.java
@@ -3,16 +3,16 @@ package swea;
 import java.io.*;
 
 public class SWEA1873 {
-
+	static char shape = 'X'; // 차의 형태
 	static char[] car = { '^', 'v', '<', '>' };
 	static char[] dir = { 'U', 'D', 'L', 'R' };
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
-	
-	static void move(int[] carPos, char shape, char dir, char[][] map) {
+
+	static void move(int[] carPos, char dir, char[][] map) {
 		if (dir == 'U') {
 			shape = car[0];
-			
+
 			map[carPos[0]][carPos[1]] = shape;
 			carPos[0] += dx[0];
 			carPos[1] += dy[0];
@@ -90,69 +90,69 @@ public class SWEA1873 {
 
 	}
 
-	static void shoot(int[] carPos, char shape, char dir, char[][] map) {
-		
-		int[] fire = {carPos[0], carPos[1]};
-		
-		if (shape == car[0]) {	//상
-			
-			while(fire[0] > 0) {
+	static void shoot(int[] carPos, char dir, char[][] map) {
+
+		int[] fire = { carPos[0], carPos[1] };
+
+		if (shape == car[0]) { // 상
+
+			while (fire[0] > 0) {
 				fire[0] += dx[0];
-				if(map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
+				if (map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
 					continue;
 				}
-				if(map[fire[0]][fire[1]] == '#') {
-					return;
+				if (map[fire[0]][fire[1]] == '#') {
+					break;
 				}
-				if(map[fire[0]][fire[1]] == '*') {
+				if (map[fire[0]][fire[1]] == '*') {
 					map[fire[0]][fire[1]] = '.';
-					return;
+					break;
 				}
 			}
-			
+
 		}
-		if (shape == car[1]) {	//하
-			while(fire[0] < map.length-1 ) {
+		if (shape == car[1]) { // 하
+			while (fire[0] < map.length - 1) {
 				fire[0] += dx[1];
-				if(map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
+				if (map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
 					continue;
 				}
-				if(map[fire[0]][fire[1]] == '#') {
-					return;
+				if (map[fire[0]][fire[1]] == '#') {
+					break;
 				}
-				if(map[fire[0]][fire[1]] == '*') {
+				if (map[fire[0]][fire[1]] == '*') {
 					map[fire[0]][fire[1]] = '.';
-					return;
+					break;
 				}
 			}
 		}
-		if (shape == car[2]) {	//좌
-			while(fire[1] > 0) {
+		if (shape == car[2]) { // 좌
+			while (fire[1] > 0) {
 				fire[1] += dy[2];
-				if(map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
+				if (map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
 					continue;
 				}
-				if(map[fire[0]][fire[1]] == '#') {
-					return;
+				if (map[fire[0]][fire[1]] == '#') {
+					break;
 				}
-				if(map[fire[0]][fire[1]] == '*') {
+				if (map[fire[0]][fire[1]] == '*') {
 					map[fire[0]][fire[1]] = '.';
-					return;
+					break;
 				}
 			}
 		}
-		if (shape == car[3]) {	//우
-			while(fire[1] < map[fire[0]].length-1) {
+		if (shape == car[3]) { // 우
+			while (fire[1] < map[fire[0]].length - 1) {
 				fire[1] += dy[3];
-				if(map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
+				if (map[fire[0]][fire[1]] == '.' || map[fire[0]][fire[1]] == '-') {
 					continue;
 				}
-				if(map[fire[0]][fire[1]] == '#') {
-					return;
+				if (map[fire[0]][fire[1]] == '#') {
+					break;
 				}
-				if(map[fire[0]][fire[1]] == '*') {
+				if (map[fire[0]][fire[1]] == '*') {
 					map[fire[0]][fire[1]] = '.';
-					return;
+					break;
 				}
 			}
 		}
@@ -177,7 +177,7 @@ public class SWEA1873 {
 				}
 
 				int[] carPosition = new int[2]; // 차의 위치
-				char shape = 'X'; // 차의 형태
+
 				for (int i = 0; i < map.length; i++) {
 					for (int j = 0; j < map[i].length; j++) {
 						if (map[i][j] == car[0] || map[i][j] == car[1] || map[i][j] == car[2] || map[i][j] == car[3]) {
@@ -187,27 +187,26 @@ public class SWEA1873 {
 						}
 					}
 				}
-				// 여기부터 구현 시작
 
 				int N = Integer.parseInt(br.readLine());
 				char[] order = br.readLine().toCharArray();
 				for (int i = 0; i < N; i++) {
-					System.out.println(i+" " + shape );
 					if (order[i] == dir[0] || order[i] == dir[1] || order[i] == dir[2] || order[i] == dir[3]) {
-						move(carPosition, shape, order[i], map);
+						move(carPosition, order[i], map);
 					}
 					if (order[i] == 'S') {
-						shoot(carPosition, shape, order[i], map);
+						shoot(carPosition, order[i], map);
 					}
 
 				}
 				for (int i = 0; i < map.length; i++) {
 					for (int j = 0; j < map[i].length; j++) {
-						System.out.print(map[i][j]);
+						sb.append(map[i][j]);
 					}
-					System.out.println();
+					sb.append("\n");
 				}
 			}
+			System.out.println(sb);
 		} catch (NumberFormatException | IOException e) {
 			e.printStackTrace();
 		}
