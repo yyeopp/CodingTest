3ae899b57c60dbeba9b8ae74bd2db88ac194d970
yyeopp <letsksy@naver.com>
2022-04-06 15:02:25 +0900

    [D4] 그래프탐색 BFS

---
+++ SWEA/Problem/SWEA5643.java
@@ -0,0 +1,89 @@
+package todo;
+
+import java.io.*;
+import java.util.*;
+
+public class SWEA5643 {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			sb.append("#").append(tc).append(" ");
+
+			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+
+			ArrayList[] nodes = new ArrayList[N + 1];
+			ArrayList[] reverse = new ArrayList[N + 1];
+			for (int i = 0; i < nodes.length; i++) {
+				nodes[i] = new ArrayList<Integer>();
+				reverse[i] = new ArrayList<Integer>();
+			}
+
+			for (int i = 0; i < M; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				nodes[start].add(end);
+				reverse[end].add(start);
+			}
+
+			int result = 0;
+
+			for (int i = 1; i < nodes.length; i++) {
+				int count = 0;
+				boolean[] checked = new boolean[N + 1];
+				Queue<Integer> queue = new LinkedList<Integer>();
+				queue.offer(i);
+
+				// 아래로
+				while (!queue.isEmpty()) {
+					int temp = queue.poll();
+					checked[temp] = true;
+					count++;
+
+					for (int j = 0; j < nodes[temp].size(); j++) {
+						int c = (int) nodes[temp].get(j);
+						if (checked[c] == false) {
+							checked[c] = true;
+							queue.offer(c);
+						}
+					}
+				}
+
+				// 위로
+				queue.clear();
+				Arrays.fill(checked, false);
+				queue.offer(i);
+
+				while (!queue.isEmpty()) {
+					int temp = queue.poll();
+					checked[temp] = true;
+					count++;
+
+					for (int j = 0; j < reverse[temp].size(); j++) {
+						int c = (int) reverse[temp].get(j);
+						if (checked[c] == false) {
+							checked[c] = true;
+							queue.offer(c);
+						}
+					}
+
+				}
+
+				if (count == N + 1) {
+					result++;
+				}
+			}
+			sb.append(result).append("\n");
+
+		}
+		System.out.println(sb);
+	}
+
+}
