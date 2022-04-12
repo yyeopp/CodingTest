27ba0f63a1d0311ba2542eac554c154f604c9c22
yyeopp <letsksy@naver.com>
2022-04-12 10:01:05 +0900

    [실버2] 에라토스테네스의 체, 베르뜨랑 공준

--- BOJ/steps/s09/boj4948.java
+++ BOJ/steps/s09/boj4948.java
@@ -4,28 +4,17 @@ import java.io.*;
 
 public class boj4948 {
 	public static boolean isPrimeNo(int n) {
-
+		boolean prime = true;
 		if (n == 1) {
 			return false;
 		}
-		if (n == 2) {
-			return true;
-		} else {
-			if (n % 2 == 0) {
-				return false;
-			} else {
-
-				for (int i = 3; i <= n / 2; i++) {
-					if (n % i == 0) {
-
-						return false;
-					}
-				}
-				return true;
-
+		for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				prime = false;
+				break;
 			}
-
 		}
+		return prime;
 
 	}
 
@@ -35,15 +24,17 @@ public class boj4948 {
 
 		while (true) {
 			int n = Integer.parseInt(br.readLine());
-			if(n==0) break;
+			if (n == 0)
+				break;
+
 			int count = 0;
-			for (int i = n; i <= 2 * n; i++) {
+			for (int i = n+1; i <= 2 * n; i++) {
 				if (isPrimeNo(i) == true) {
 					count++;
 				}
 
 			}
-			sb.append(count+"\n");
+			sb.append(count + "\n");
 		}
 		System.out.println(sb);
 
