3ab6c9691bf05a81b81cf33c1ebbfacf2426110d
yyeopp <letsksy@naver.com>
2022-04-12 09:51:04 +0900

    [실버3] 에라토스테네스의 체

--- BOJ/steps/s09/boj1929.java
+++ BOJ/steps/s09/boj1929.java
@@ -1,53 +1,41 @@
 package steps.s09;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class boj1929 {
 	public static boolean isPrimeNo(int n) {
-
-		if (n == 1) {
+		if (n == 1)
 			return false;
-		}
-		if (n == 2) {
-			return true;
-		} else {
-			if (n % 2 == 0) {
-				return false;
-			} else {
-
-				for (int i = 3; i <= n / 2; i++) {
-					if (n % i == 0) {
-
-						return false;
-					}
-				}
-				return true;
-
+		boolean prime = true;
+		for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+			// 에라토스테네스의 체 원리 상, 특정 자연수 n에 대한 소수 여부 검증은 
+			// sqrt(n) 까지 수에 대해 mod 연산을 수행해보는 것으로 충분하다.
+			if (n % i == 0) {
+				prime = false;
+				break;
 			}
-
 		}
+
+		return prime;
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 
-		try {
-			String[] temp = br.readLine().split(" ");
-			int[] tc = new int[2];
-			int count = 0;
-			for (int i = 0; i < tc.length; i++) {
-				tc[i] = Integer.parseInt(temp[i]);
-				if (boj1929.isPrimeNo(tc[i]) == true) {
-					count++;
-				}
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = M; i <= N; i++) {
+			if (isPrimeNo(i)) {
+				sb.append(i).append("\n");
 			}
-			System.out.println(count);
-
-		} catch (NumberFormatException | IOException e) {
-			e.printStackTrace();
 		}
-	}
+		System.out.println(sb);
 
+	}
 }
