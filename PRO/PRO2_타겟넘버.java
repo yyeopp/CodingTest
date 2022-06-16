4bf404d7797a7528f325959e7cd9c087f34cda87
yyeopp <letsksy@naver.com>
2022-06-16 14:06:14 +0900

    [레벨2] 간단한 DFS

--- PRO/PRO2_타겟넘버.java
+++ PRO/PRO2_타겟넘버.java
@@ -1,5 +1,6 @@
 package pro;
 
+// 매우 간단한 DFS 문제
 public class PRO2_타겟넘버 {
 
 	static int answer;
@@ -10,12 +11,14 @@ public class PRO2_타겟넘버 {
 		int[] numbers = { 4, 1, 2, 1 };
 		int target = 4;
 
+		// dfs
 		dfs(numbers, target, 0);
 		System.out.println(answer);
 	}
 
 	private static void dfs(int[] numbers, int target, int idx) {
 
+		// 기저조건
 		if (idx == numbers.length) {
 			if (sum == target) {
 				answer++;
@@ -23,10 +26,13 @@ public class PRO2_타겟넘버 {
 			return;
 		}
 
+		// 기존 sum에 numbers 배열의 원소를 '더하고'
 		sum += numbers[idx];
 		dfs(numbers, target, idx + 1);
+		// 그 다음은 '빼고' (아까 더했으므로 -2*numbers[idx] 해줘야 빼는 효과)
 		sum -= 2 * numbers[idx];
 		dfs(numbers, target, idx + 1);
+		// 갖다 왔으니까 원상복귀시킴
 		sum += numbers[idx];
 	}
 
