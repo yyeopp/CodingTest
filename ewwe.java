79d4a7b98bb7d5d9bdcac70ae6b78bcd68ad82c6
yyeopp <letsksy@naver.com>
2022-06-25 11:43:40 +0900

    .

---
+++ ewwe.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class M1 {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		int n = 3;
+		int m = 3;
+		int[] x = { 1, 1, 2 };
+		int[] y = { 3, 2, 3 };
+		int[] z = { 1, 5, 2 };
+
+		int[] answer = new int[m];
+
+		// 맵 세팅
+		int[][] map = new int[n][n];
+
+		for (int i = 0; i < m; i++) {
+			map[x[i] - 1][y[i] - 1] = z[i];
+			map[y[i] - 1][x[i] - 1] = z[i];
+		}
+
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				if (map[i][j] == 0) {
+					map[i][j] = 200000;
+				}
+			}
+		}
+		for (int i = 0; i < map.length; i++) {
+			map[i][i] = 0;
+		}
+		// 최단경로들
+		ArrayList[][] list = new ArrayList[m][m];
+		for (int i = 0; i < list.length; i++) {
+			for (int j = 0; j < list.length; j++) {
+				list[i][j] = new ArrayList<>();
+			}
+		}
+		for (int i = 0; i < m; i++) {
+			list[x[i] - 1][y[i] - 1].add(i + 1);
+			list[y[i] - 1][x[i] - 1].add(i + 1);
+		}
+
+		// 플로이드-워셜
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					if (map[j][k] > map[j][i] + map[i][k]) {
+						map[j][k] = map[j][i] + map[i][k];
+						list[j][k].clear();
+						list[j][k].addAll(list[j][i]);
+						list[j][k].addAll(list[i][k]);
+					}
+				}
+			}
+		}
+
+		Map<Integer, Integer> result = new HashMap<Integer, Integer>();
+		for (int i = 0; i < m; i++) {
+			result.put(i, 0);
+		}
+		for (int i = 0; i < list.length; i++) {
+			for (int j = 0; j < list[i].length; j++) {
+				for (int k = 0; k < list[i][j].size(); k++) {
+					result.put((int) list[i][j].get(k) - 1, result.get((int) list[i][j].get(k) - 1) + 1);
+				}
+			}
+		}
+
+		List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(result.entrySet());
+
+		entryList.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+			@Override
+			public int compare(Map.Entry<Integer, Integer> o1,
+					Map.Entry<Integer, Integer> o2) {
+				if (o1.getValue() != o2.getValue()) {
+					return o2.getValue() - o1.getValue();
+				} else {
+					return o1.getKey() - o2.getKey();
+				}
+			}
+		});
+
+		for (int i = 0; i < entryList.size(); i++) {
+			answer[i] = entryList.get(i).getKey() + 1;
+		}
+		System.out.println(Arrays.toString(answer));
+
+	}
+
+}
